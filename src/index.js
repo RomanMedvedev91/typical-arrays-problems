be00c9b2fd0d56b277b6dc8b1b3143450766e0ad
Roman Medvedev <roman.medvedev77@gmail.com>
2020-10-03 20:37:13 +0500

    feat: add functions into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    if (array === undefined) return 0;
+    return array.length > 1 ? array.sort((a, b) => a - b)[0] : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined) return 0;
+    return array.length > 1 ? array.sort((a, b) => b - a)[0] : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined) return 0;
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    if (array.length === 0) return 0;
+    return (total / array.length).toFixed(2);
+};
